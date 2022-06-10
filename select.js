3322c6aaba1bf0ea7708bb46046ebbc5d8590c4b
Python125 <91922582+Python125@users.noreply.github.com>
2022-06-10 10:45:36 -0400

    Create select.js

---
+++ select.js
@@ -0,0 +1,11 @@
+class Select {
+    constructor (opts = {
+        question: "Would you like to convert to celsius or fahrenheit?",
+        options: ["Celsius", "Fahrenheit"],
+        answers: ["celsius", "fahrenheit"],
+        pointer: ">"
+    }) {
+        
+    }
+
+}
